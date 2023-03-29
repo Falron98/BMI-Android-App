7dc1dccc8f2fbee0fd17d6e4398d37d9687cd38e
Falron98 <bartekkrystowski@wp.pl>
2023-03-29 09:53:34 +0200

    Added starting splash screen

---
+++ app/src/main/java/com/example/bmi_calc/StartingScreen.java
@@ -0,0 +1,27 @@
+package com.example.bmi_calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StartingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_screen);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(StartingScreen.this, MainActivity.class));
+                finish();}
+        }, 3000);
+    }
+}
