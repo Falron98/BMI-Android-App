9ed5323a2a98f53914cfe12b8c49ddab9e7e88d5
Falron98 <bartekkrystowski@wp.pl>
2023-04-19 16:49:06 +0200

    Added quiz function, added button for quiz function, added quiz layout, added border to quiz layout, added example of questions for quiz

--- app/src/main/java/com/example/bmi_calc/Buttons.java
+++ app/src/main/java/com/example/bmi_calc/Buttons.java
@@ -11,6 +11,7 @@ public class Buttons extends AppCompatActivity {
     private Button bmi_move;
     private Button cls_move;
     private Button rcp_move;
+    private Button quiz_move;
 
 
     @Override
@@ -20,6 +21,7 @@ public class Buttons extends AppCompatActivity {
         bmi_move = findViewById(R.id.button_BMI_Calculator);
         cls_move = findViewById(R.id.button_Cls_Calculator);
         rcp_move = findViewById(R.id.button_Recipes);
+        quiz_move = findViewById(R.id.button_Quiz);
         bmi_move.setOnClickListener(view -> {
             Intent intent = new Intent(Buttons.this, BMI_Calculator.class);
             startActivity(intent);
@@ -30,6 +32,10 @@ public class Buttons extends AppCompatActivity {
         });
         rcp_move.setOnClickListener(view -> {
             Intent intent = new Intent(Buttons.this, Recipes.class);
+            startActivity(intent);
+        });
+        quiz_move.setOnClickListener(view -> {
+            Intent intent = new Intent(Buttons.this, Quiz.class);
             startActivity(intent);
         });
     }
